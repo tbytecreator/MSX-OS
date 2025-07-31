7caec7d631e8342e6ea23ae99e01d03e4edd7a4e
Manoel Neto <113383491+tbytecreator@users.noreply.github.com>
2025-07-31 19:52:08 -0300

    diferenças do reboot.bat

--- BATCH/make.bat
+++
@@ -1,6 +0,0 @@
-
-@echo off
-:Emulator
-Set MyProcess=openmsx.exe
-tasklist | find /i "%MyProcess%">nul  && (echo %MyProcess% Already running) || start openmsx.exe -script openMSX\1-emul_start_MSX1_config.txt
-:_end_
